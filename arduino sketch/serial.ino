3260556539ba8464a9172bf199cb8f6f8519f6a6
ni_n_ja <rampage4extrm@gmail.com>
2020-10-22 17:00:05 +0900

    enable 2 Serrial Ports

--- arduino sketch/serial.ino
+++ arduino sketch/serial.ino
@@ -1,36 +1,63 @@
 #include <Keyboard.h>
 
-void setup(void) {
+void setup(void)
+{
   Serial.begin(9600);
+  Serial1.begin(9600);
   Keyboard.begin();
 }
 
-void keyWait() {
-  while (Serial.available() <= 0)
+void keyWait()
+{
+  while (Serial.available() <= 0 || Serial.available() <= 0)
     delay(1);
 }
 
-void loop(void) {
+void loop(void)
+{
   char buf[1024];
   byte ch;
-  
+
   keyWait();
-  byte tp = Serial.read();
-  switch (tp) {
+  byte tp;
+  if (0 < Serial.available())
+  {
+    tp = Serial.read();
+  }
+  else if (0 < Serial1.available())
+  {
+    tp = Serial1.read();
+  }
+  switch (tp)
+  {
   case 1:
     keyWait();
-    ch = Serial.read();
+    if (0 < Serial.available())
+    {
+      ch = Serial.read();
+    }
+    else if (0 < Serial1.available())
+    {
+      ch = Serial1.read();
+    }
     Keyboard.press(ch);
     sprintf(buf, "%02x", ch);
-    Serial.println(buf);
+    // Serial.println(buf);
     break;
 
   case 2:
     keyWait();
-    ch = Serial.read();
+    if (0 < Serial.available())
+    {
+      ch = Serial.read();
+    }
+    else if (0 < Serial1.available())
+    {
+      ch = Serial1.read();
+    }
     Keyboard.release(ch);
     sprintf(buf, "%02x", ch);
-    Serial.println(buf);
+    // Serial.println(buf);
     break;
   }
 }
